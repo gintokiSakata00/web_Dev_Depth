9eb75814b3943aba654088d141c46cd4069f2fcf
gintokiSakata00 <gillbertpadon01@gmail.com>
2022-11-29 12:31:12 +0800

    done html

--- learning_html/index.js
+++
@@ -1,7 +0,0 @@
-var name = "Gillbert Padon"
-var password = 202080194
-var vault = {names:name, pass:password, email:String(password+"@psu.palawan.edu.ph")}
-console.log(`
-Name: ${vault.names}
-Password: ${vault.pass}
-Email: ${vault.email}`)
